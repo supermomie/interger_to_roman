f13298c84be280276886c68ad1754e1297c13f87
supermomie <fakhredinedev3.0@gmail.com>
2019-09-19 03:19:20 +0200

    starting INTEGER TO ROMAN

---
+++ integer_to_Roman.py
@@ -0,0 +1,26 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+
+    def romanNumbers(self, val):
+        roman_numbers = {"I" : 1,
+                        "V" : 5,
+                        "X" : 10,
+                        "L" : 50,
+                        "C" : 100,
+                        "D" : 500,
+                        "M" : 1000}
+
+    def goingToTarget(self, number):
+        #TODO se raproche du maximum du nombre
+
+    def calcul(self):
+        #TODO 68 = 50 + 10 + 5 + 1 + 1 + 1
+
+
+    def checkCombination(self, num):
+        #TODO check combination
+
+
+sol = Solution()
+print(sol.intToRoman(1992))
